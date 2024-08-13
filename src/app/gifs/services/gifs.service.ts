8162218ccd351e61e5e6fbf95b2f514c3969db30
Michelle Champney <champney.miichelle9619@gmail.com>
2024-08-13 16:38:46 -0600

    lazy image

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -46,7 +46,7 @@ export class GifsService {
 
     const params = new HttpParams()
       .set('api_key', this.apiKey)
-      .set('limit', 10)
+      .set('limit', 20)
       .set('q', tag);
 
     this.http
